4d45b3081d6d152f523e8be2022d258886a7c1ca
Arif <homearif6@gmail.com>
2025-06-28 22:55:40 +0700

    Fix Spotify image domain configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     transpilePackages: ['next-mdx-remote'],
+    images: {
+        domains: ['i.scdn.co'],
+    },
 };
 
 export default nextConfig;
