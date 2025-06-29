dc6cd9d258dbf337b06470bee79140cbadbd2680
Arif <homearif6@gmail.com>
2025-06-29 09:33:36 +0700

    Updated site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
     title: 'Arif (@masarif.id)',
-    description: 'Photographer based in Central Java, Indonesia.',
+    description: 'A visual storyteller from Central Java, Indonesia who finds joy in creating art, playing with the camera, canvas and colors, traveling, filming and documenting moments along the way.',
     url: 'https://masarif.id',
     ogImage: 'https://masarif.id/images/og-image.png',
     author: 'Arif',
